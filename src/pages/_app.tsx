2b56947683466785b2ee5fb0bbd06f6ffd8581e0
Maxence <maxence@MacBook-Pro-de-Maxence.local>
2021-07-14 13:31:30 +0200

    fix: added google anayltics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,11 @@
 import AppType, { AppContext, AppInitialProps } from 'next/app'
 import { withUserAgent, WithUserAgentProps } from 'next-useragent'
 import React from 'react'
+import { NextWebVitalsMetric } from 'next/app'
 
 import { Page } from '@src/utils/libs/nextjs'
 import { GlobalStyles } from '@src/utils/styles/Global'
+import Head from 'next/head'
 
 class App extends AppType<AppContext & WithUserAgentProps & {}, AppInitialProps> {
   static async getInitialProps(ctx: any) {
@@ -28,6 +30,22 @@ class App extends AppType<AppContext & WithUserAgentProps & {}, AppInitialProps>
 
     return (
       <>
+        <Head>
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script async src={`https://www.googletagmanager.com/gtag/js?id=G-HEX2M4ZB52`} />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-HEX2M4ZB52', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
+        </Head>
         <GlobalStyles />
         <Layout>
           <Component {...pageProps} />
@@ -35,6 +53,15 @@ class App extends AppType<AppContext & WithUserAgentProps & {}, AppInitialProps>
       </>
     )
   }
+}
+
+export const reportWebVitals = ({ id, name, label, value }: NextWebVitalsMetric) => {
+  ;(window as any).gtag('event', name, {
+    event_category: label === 'web-vital' ? 'Web Vitals' : 'Next.js custom metric',
+    value: Math.round(name === 'CLS' ? value * 1000 : value), // values must be integers
+    event_label: id, // id unique to current page load
+    non_interaction: true, // avoids affecting bounce rate.
+  })
 }
 
 export default withUserAgent(App)
